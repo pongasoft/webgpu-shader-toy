57981a33a4484d3cc9dd15ecc4bb2fd1fdae6ee0
Yan Pujante <yan@pongasoft.com>
2024-04-30 13:21:36 -0700

    default size driven by css

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <backends/imgui_impl_glfw.h>
 #include <emscripten.h>
+#include <emscripten/html5.h>
 #include "Preferences.h"
 #include "Application.h"
 #include "MainWindow.h"
@@ -28,10 +29,13 @@ int main(int, char **)
   std::shared_ptr<shader_toy::Preferences> preferences =
     std::make_unique<shader_toy::Preferences>(std::make_unique<utils::JSStorage>());
 
+  double width, height;
+  emscripten_get_element_css_size("#canvas2", &width, &height);
+
   kApplication
     ->registerRenderable<shader_toy::FragmentShaderWindow>(Window::Args{
                                                              .size = preferences->loadSize(shader_toy::FragmentShaderWindow::kPreferencesSizeKey,
-                                                                                           {380, 380}),
+                                                                                           {static_cast<int>(width), static_cast<int>(height)}),
                                                              .title = "WebGPU Shader Toy | fragment shader",
                                                              .canvas = {
                                                                .selector = "#canvas2",
@@ -45,10 +49,12 @@ int main(int, char **)
                                                            })
     ->show();
 
+  emscripten_get_element_css_size("#canvas1", &width, &height);
+
   kApplication
     ->registerRenderable<shader_toy::MainWindow>(Window::Args{
                                                    .size = preferences->loadSize(shader_toy::MainWindow::kPreferencesSizeKey,
-                                                                                 {520, 600}),
+                                                                                 {static_cast<int>(width), static_cast<int>(height)}),
                                                    .title = "WebGPU Shader Toy",
                                                    .canvas = {
                                                      .selector = "#canvas1",
